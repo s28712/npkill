c0e7ad79fbb8594e81b7a903db54af895ec5404d
simar-singh <simar.singh.r@gmail.com>
2021-11-25 16:50:03 -0500

    Delete all node_module folders
    Created basic function using os to walk through all directories and search for node_modules, and then using shutil remove the directory.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+# Import os to get current directory and subdirectories
 import os
- 
-def main():
-  print("Hello World")
+# Import shutil to remove a directory and all contents
+import shutil
 
+# Main function
+def main():
+  # Get current directory
+  directory = os.getcwd()
+  # Crawl through all subdirectories
+  for root, subdirectories, files in os.walk(directory):
+    for subdirectory in subdirectories:
+      # If the subdirectory is node_modules, remove directory
+      if subdirectory == "node_modules":
+        shutil.rmtree(os.path.join(root, subdirectory))
+        
 if __name__ == '__main__':
   main()
